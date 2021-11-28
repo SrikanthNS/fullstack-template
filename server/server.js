2972016c22d535b2aeb0a10e4a4b01b17e5655b7
Srikanth N S <7464926+SrikanthNS@users.noreply.github.com>
2021-11-28 14:22:37 +0530

    add backend auth routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -8,10 +9,36 @@ var corsOptions = {
     origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
 };
 
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const db = require("./app/models");
+const Role = db.role;
+
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
+    initial();
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -22,9 +49,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to fullstack-server-side application." });
 });
 
+
+// Auth routes
+require('./app/routes/auth.routes')(app);
+// User routes
+require('./app/routes/user.routes')(app);
+
+// Tutorial routes
 require("./app/routes/turorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
